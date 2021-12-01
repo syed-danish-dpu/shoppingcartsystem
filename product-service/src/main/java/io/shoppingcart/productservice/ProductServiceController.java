808ee81e5e256513b6dd8b85fe02aca3fec87e49
sydanish <sydanish@LIN25007688.corp.capgemini.com>
2021-12-01 16:05:46 +0530

    Shopping Cart System

--- product-service/src/main/java/io/shoppingcart/productservice/ProductServiceController.java
+++ product-service/src/main/java/io/shoppingcart/productservice/ProductServiceController.java
@@ -1,12 +1,7 @@
 package io.shoppingcart.productservice;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class ProductServiceController {
 
-		@GetMapping("/product")
 		public String WelcomeProduct() {
 				return "Hello Product";
 		}
