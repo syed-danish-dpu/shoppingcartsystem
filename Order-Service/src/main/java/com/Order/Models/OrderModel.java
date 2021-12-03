f1a6196fbb336434fd65c371facfcf031f61ec84
sydanish <sydanish@LIN25007688.corp.capgemini.com>
2021-12-03 08:57:47 +0530

    Shopping Cart System

--- Order-Service/src/main/java/com/Order/Models/OrderModel.java
+++ Order-Service/src/main/java/com/Order/Models/OrderModel.java
@@ -3,7 +3,9 @@ package com.Order.Models;
 import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection="orderModel")
 public class OrderModel {
 	
 	@Id
